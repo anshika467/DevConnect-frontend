bf6a5ab01a71b36d4c4d253810c62f309c1307ef
Anshika Saxena <developer.anshika467@gmail.com>
2025-07-25 17:59:49 +0530

    Redirect-to-login-if_not-autheticated

--- README.md
+++ README.md
@@ -35,4 +35,9 @@ Outlet => Any children routes of body will be rendered over here.
  - Login and see if your data is coming properly in the store
  - NavBar should update as soon as user logs in
  - Refactor our code to add constants file + create a components folder
- 
+ - You should not be able to access other routes without login
+ - If token is not present, redirect user to login page
+ - Logout
+ - Profile Page
+
+ 401 => Unauthorised
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
@@ -7,7 +8,7 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">👩🏻‍💻 DevTinder</a>
+        <Link to="/" className="btn btn-ghost text-xl">👩🏻‍💻 DevTinder</Link>
       </div>
       {user && (
         <div className="flex gap-2">
@@ -24,13 +25,13 @@ const NavBar = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-200 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-gradient-to-tr from-gray-950 via-blue-900 to-gray-950 rounded-box z-1 mt-4 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
