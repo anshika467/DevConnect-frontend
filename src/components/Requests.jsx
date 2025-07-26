416292797f5fc6ce02649d935d35436b0341f73e
Anshika Saxena <developer.anshika467@gmail.com>
2025-07-26 15:29:44 +0530

    Accept-Reject-Request

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -2,11 +2,26 @@ import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequest } from "../utils/RequestSlice";
+import { addRequest, removeRequest } from "../utils/RequestSlice";
 
 const Requests = () => {
   const dispatch = useDispatch();
   const requests = useSelector((store) => store.requests);
+
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(removeRequest(_id));
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const fetchRequests = async () => {
     try {
@@ -26,7 +41,8 @@ const Requests = () => {
 
   if (!requests) return;
 
-  if (requests.length === 0) return <h1>No Requests Found</h1>;
+  if (requests.length === 0)
+    return <h1 className="flex justify-center my-10">No Requests Found</h1>;
 
   return (
     <div className="flex flex-col items-center my-10 gap-4">
@@ -39,7 +55,7 @@ const Requests = () => {
         return (
           <div
             key={_id}
-            className="flex justify-between items-center m-2 p-4 rounded-lg bg-base-300 w-2/3"
+            className="flex items-center m-2 p-4 rounded-lg bg-base-300 w-1/2"
           >
             <div>
               <img
@@ -48,16 +64,28 @@ const Requests = () => {
                 alt="User_photo"
               />
             </div>
-            <div className="text-left mx-6">
-              <h2 className="font-bold text-xl">
-                {firstName + " " + lastName}
-              </h2>
-              {age && gender && <p>{age + ", " + gender}</p>}
-              <p>{about}</p>
-            </div>
-            <div className="flex gap-2">
-              <button className="btn btn-soft btn-success">Accept</button>
-              <button className="btn btn-soft btn-error">Reject</button>
+            <div className="flex justify-between w-full items-center">
+              <div className="text-left mx-6">
+                <h2 className="font-bold text-xl">
+                  {firstName + " " + lastName}
+                </h2>
+                {age && gender && <p>{age + ", " + gender}</p>}
+                <p>{about}</p>
+              </div>
+              <div className="flex gap-2">
+                <button
+                  className="btn btn-soft btn-success"
+                  onClick={() => reviewRequest("accepted", request._id)}
+                >
+                  Accept
+                </button>
+                <button
+                  className="btn btn-soft btn-error"
+                  onClick={() => reviewRequest("rejected", request._id)}
+                >
+                  Reject
+                </button>
+              </div>
             </div>
           </div>
         );
