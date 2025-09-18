4025801be877770b237a4dfadb9b702c8a31918c
Anshika Saxena <developer.anshika467@gmail.com>
2025-09-18 13:09:36 +0530

    Deployment Completed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "/api";  // The front IP part is automatically taken by the server.
+export const BASE_URL = "/api";  // The front IP part is automatically taken by the server.
 
-export const BASE_URL = "http://localhost:7777"; // For local testing
+// export const BASE_URL = "http://localhost:7777"; // For local testing
